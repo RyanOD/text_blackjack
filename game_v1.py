7cbc2d35d94781619893748d770f3beae1d8961b
Ryan <ryan.odonnell@gmail.com>
2017-12-12 08:30:48 -0800

    First commit of Blackjack game.

---
+++ game_v1.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import random
+
+os.system('clear')
+
+cards = {
+	2 	: 2,
+	3		: 3,
+	4		: 4,
+	5		:	5,
+	6		:	6,
+	7		: 7,
+	8		: 8,
+	9		: 9,
+	10	:	10,
+	11	: "Jack",
+	12	: "Queen",
+	13	: "King",
+	14	: "Ace"
+}
+
+hands = {
+	'player' : [],
+	'dealer' : []
+}
+
+def deal(hand):
+	for x in range(0,2):
+		hand.append(draw())
+
+def draw():
+	return(cards[random.randint(2,14)])
+
+def show(player,hand,show):
+	print(player,end='')
+	for card in range(0,show):
+		print(card)
+
+deal(hands['player'])
+deal(hands['dealer'])
+
+show('Dealer',hands['dealer'],1)
+show('Player',hands['player'],2)

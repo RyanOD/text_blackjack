83fc5a176944e17fbcbeb580be62fb05aaf0563e
Ryan <ryan.odonnell@gmail.com>
2017-12-19 16:57:59 -0800

    Updating with random.shuffle

--- game_v1.py
+++ game_v1.py
@@ -5,78 +5,20 @@ import math
 
 os.system('clear')
 
-suits = ['Spades', 'Diamonds', 'Hearts', 'Clubs']
-suits_symbols = ['♠', '♦', '♥', '♣']
+ranks = [_ for _ in range(2,11)] + ['Jack', 'Queen', 'King', 'Ace']
+suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+suits_symbols = {'Clubs' : '♣', 'Diamonds' : '♦', 'Hearts' : '♥', 'Spades' : '♠'}
 
-cards = {
-	2 	: 2,
-	3		: 3,
-	4		: 4,
-	5		:	5,
-	6		:	6,
-	7		: 7,
-	8		: 8,
-	9		: 9,
-	10	:	10,
-	11	: "Jack",
-	12	: "Queen",
-	13	: "King",
-	14	: "Ace"
-}
+def get_deck():
+	return [[rank, suit] for rank in ranks for suit in suits]
 
-hands = {
-	'player' : [],
-	'dealer' : []
-}
+def deal_card(deck):
+	return deck.pop()
 
-def title():
-	print("""
-		*******************************
-		*                             *
-		*                             *
-		*                             *
-		*    Welcome to Blackjack!    *
-		*                             *
-		*                             *
-		*                             *
-		*      ♠    ♦     ♥    ♣      *
-		*                             *
-		*                             *
-		*   Press 'enter' to begin    *
-		*                             *
-		*                             *
-		*      ♠    ♦     ♥    ♣      *
-		*                             *
-		*                             *
-		*                             *
-		*******************************
-		""")
+deck = get_deck()
+random.shuffle(deck)
 
-	input()
+player_hand = [deal_card(deck), deal_card(deck)]
+dealer_hand = [deal_card(deck), deal_card(deck)]
 
-def deal(hand):
-	for x in range(0,2):
-		hand.append(draw())
-
-def draw():
-	return(cards[random.randint(2,14)])
-
-def show(hand,show):
-	for card in hand:
-		print(card,end=', ')
-
-title()
-
-deal(hands['player'])
-deal(hands['dealer'])
-
-print('Player: ',end='')
-show(hands['player'],2)
-
-# check for natural blackjack
-print('\n')
-
-print('Dealer: ',end='')
-show(hands['dealer'],2)
-
-print('\n')
+print(player_hand)
